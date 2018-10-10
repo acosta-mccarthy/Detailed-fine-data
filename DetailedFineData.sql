5571a8fe94f8847ff7606718b27b4644ee0ae8a2
Nina Acosta & Eric McCarthy <nacosta@midhudson.org>
2018-10-10 13:44:41 -0400

    Update DetailedFineData.sql

--- DetailedFineData.sql
+++ DetailedFineData.sql
@@ -1,25 +1,25 @@
-
-
-
-SELECT
-field_name,
-TO_CHAR (date_field_gmt, 'MM/DD/YYYY') AS "Date",
-CONCAT (field_one, field_two)
---Add comments to your code
-
+SELECT 
+  CONCAT ('p', patron_view.record_num, 'a') AS "Record Number", --adds p prefix to the patron record number and the "a" as a placeholder for the check digit, so the number can be easily copied and pasted into Sierra
+  CONCAT (patron_record_fullname.first_name, ' ', patron_record_fullname.middle_name, ' ', patron_record_fullname.last_name) AS "Name", --Combines parts of patron name into First-Middle-Last format
+  TO_CHAR(fine.assessed_gmt, 'MM/DD/YYYY') AS "Date Assessed",--Formats the date field
+  fine.charge_location_code AS "Location Code", 
+  ROUND ((fine.item_charge_amt + fine.billing_fee_amt),2) AS "Amount Owed",--Combines fees, rounds to 2 decimal places
+  CONCAT (fine.title, fine.description) AS "Title" --Combines bib record title, or manual fee note into one column
+   
 FROM
-  sierra_view.table_one
-JOIN sierra_view.table_two
- ON table_one.field = table_two.field
+  sierra_view.fine
+INNER JOIN sierra_view.patron_view
+ON  patron_view.id = fine.patron_record_id
+INNER JOIN sierra_view.patron_record_fullname
+ON patron_record_fullname.patron_record_id = patron_view.id
 
 WHERE
-date_created BETWEEN '10/01/2018' AND CURRENT_DATE
-field = 'value'
-other_field LIKE "%text%"
+--Replace "abc" below with library's three letter code
+  fine.charge_location_code LIKE 'lag%' AND 
+--Replace "01/01/2018" with beginning date 
+  fine.assessed_gmt BETWEEN '01/01/2018' AND CURRENT_DATE-- CURRENT_DATE can be replaced with a numeric date if desired
 
-ORDER BY
-patron_record_number, date, title
-
+ORDER BY patron_view.record_num, Title;--Sorts by patron record number, then title - patrons with multiple fines will have them listed in alphabetical order
 
 
 
